446f4b3d0e9feafb95602cc9893e77c8a29366c5
Saharak <saharak@bananacoding.com>
2019-09-21 00:05:32 +0700

    [Updated] code

--- android/app/src/main/java/com/dprint/MainApplication.java
+++ android/app/src/main/java/com/dprint/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.proyecto26.inappbrowser.RNInAppBrowserPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.react.ReactNativeHost;
--- src/modules/Navigator/NavigatorView.js
+++ src/modules/Navigator/NavigatorView.js
@@ -1,14 +1,70 @@
 import * as React from 'react';
 import {
+  View,
+  Alert,
+} from 'react-native';
+import {
   BottomNavigation,
-  Text
+  Text,
+  Button
 } from 'react-native-paper';
-
-const MusicRoute = () => <Text>Music</Text>;
-const AlbumsRoute = () => <Text>Albums</Text>;
-const RecentsRoute = () => <Text>Recents</Text>;
+import InAppBrowser from 'react-native-inappbrowser-reborn'
 
 const NavigatorView = () => {
+  MusicRoute = () => {
+    return (
+      <View>
+        <Text>Musicdd</Text>
+        <Button onPress={this.openLink()}>Test</Button>
+      </View>
+    );
+  }
+
+  openLink = async () => {
+    try {
+      const url = 'https://www.google.com'
+      if (await InAppBrowser.isAvailable()) {
+        const result = await InAppBrowser.open(url, {
+          // iOS Properties
+          dismissButtonStyle: 'cancel',
+          preferredBarTintColor: '#453AA4',
+          preferredControlTintColor: 'white',
+          readerMode: false,
+          animated: true,
+          modalPresentationStyle: 'overFullScreen',
+          modalTransitionStyle: 'partialCurl',
+          modalEnabled: true,
+          // Android Properties
+          showTitle: true,
+          toolbarColor: '#6200EE',
+          secondaryToolbarColor: 'black',
+          enableUrlBarHiding: true,
+          enableDefaultShare: true,
+          forceCloseOnRedirection: false,
+          // Specify full animation resource identifier(package:anim/name)
+          // or only resource name(in case of animation bundled with app).
+          animations: {
+            startEnter: 'slide_in_right',
+            startExit: 'slide_out_left',
+            endEnter: 'slide_in_left',
+            endExit: 'slide_out_right'
+          },
+          headers: {
+            'my-custom-header': 'my custom header value'
+          },
+          waitForRedirectDelay: 0
+        })
+        Alert.alert(JSON.stringify(result))
+      }
+      else Linking.openURL(url)
+    } catch (error) {
+      Alert.alert(error.message)
+    }
+  }
+
+  const AlbumsRoute = () => <Text>Albums</Text>;
+  const RecentsRoute = () => <Text>Recents</Text>;
+
   state = {
     index: 0,
     routes: [
